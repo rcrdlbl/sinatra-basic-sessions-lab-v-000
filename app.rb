f99c9bdbf749a49033f50e3d79b13459f6851fa0
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-25 22:47:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "sseeccrreett"
+  end
+
+  get '/' do
+    erb :index
+  end
 end
